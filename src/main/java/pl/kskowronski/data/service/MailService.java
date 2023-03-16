e7bb0906bf33436f48e16647c959ee4430c2e101
k.skowronski <claude-plos@o2.pl>
2023-03-16 13:31:09 +0100

    update mail trust ssl

--- src/main/java/pl/kskowronski/data/service/MailService.java
+++ src/main/java/pl/kskowronski/data/service/MailService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 @Service
 public class MailService {
